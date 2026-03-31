adc8fdc7ca5ac63e7bd00ef64eb39adbb471c302
Andrey Helldar <helldar@dragon-code.pro>
2026-03-31 16:28:18 +0300

    Clean up tests

--- tests/Unit/CustomTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// Custom tests have been moved to tests/Unit/Request/CustomTest.php.
--- tests/Unit/RequestTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// Tests for TelemetryRequest have been split into grouped files under tests/Unit/Request.
